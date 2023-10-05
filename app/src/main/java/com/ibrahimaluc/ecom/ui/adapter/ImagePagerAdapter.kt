0baf441969138a71f0af0c4b40ae47228959a8b1
ibrahimaluc <ibrahimaluc@ogr.iu.edu.tr>
2023-10-05 18:02:08 +0300

    xmls changed.

--- app/src/main/java/com/ibrahimaluc/ecom/ui/adapter/ImagePagerAdapter.kt
+++ app/src/main/java/com/ibrahimaluc/ecom/ui/adapter/ImagePagerAdapter.kt
@@ -3,10 +3,6 @@ package com.ibrahimaluc.ecom.ui.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
@@ -18,11 +14,10 @@ import com.ibrahimaluc.ecom.databinding.ItemImageCarouselBinding
 class ImagePagerAdapter(
     private val context: Context,
     private val imageList: List<String>,
+    private val onLikeClickListener: (position: Int) -> Unit,
+    private val favoriteStatusList: List<Boolean>
 
     ) : RecyclerView.Adapter<ImagePagerAdapter.ImagePagerViewHolder>() {
-
-    private var selectedPosition = 0
-    private lateinit var buttonContainer: LinearLayout
 
     class ImagePagerViewHolder(var binding: ItemImageCarouselBinding) : ViewHolder(binding.root)
 
@@ -45,44 +40,20 @@ class ImagePagerAdapter(
             .apply(requestOptions)
             .into(holder.binding.ivProductImage)
 
+        val isLiked = favoriteStatusList[position]
+        if (isLiked) {
+            holder.binding.ibLike.setImageResource(R.drawable.icon_favorite_filled)
+        } else {
+            holder.binding.ibLike.setImageResource(R.drawable.icon_favorite_passive)
+        }
+
+        holder.binding.ibLike.setOnClickListener {
+           onLikeClickListener(position)
+        }
 
     }
 
     override fun getItemCount(): Int {
         return imageList.size
-    }
-
-    fun setButtonContainer(container: LinearLayout) {
-        buttonContainer = container
-        createButtons()  // Butonları oluştur
-    }
-
-    private fun createButtons() {
-        for (i in imageList.indices) {
-            val button = Button(context)
-            button.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            button.text = (i + 1).toString()
-            button.setOnClickListener {
-                setSelectedPosition(i)
-            }
-            buttonContainer.addView(button)
-        }
-        setSelectedPosition(0)
-    }
-
-    private fun setSelectedPosition(position: Int) {
-        selectedPosition = position
-        notifyDataSetChanged()
-        updateButtonStates()
-    }
-
-    private fun updateButtonStates() {
-        for (i in 0 until buttonContainer.childCount) {
-            val button = buttonContainer.getChildAt(i) as Button
-            button.isSelected = i == selectedPosition
-        }
     }
 }
